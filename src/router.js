8b7d72b5674b44dbebcd406bc0d9dd12c6269e49
MauricioCortess <maucortes2001@gmail.com>
2025-11-18 03:06:14 -0500

    Arreglo router para GitHub Pages

--- src/router.js
+++ src/router.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './pages/Home.vue'
-import ExploraAfrica from './components/ExploraAfrica.vue'
-import ContactoForm from './components/ContactoForm.vue'
 import SafariPlaya from './pages/SafariPlaya.vue'
 import SobreNosotros from './pages/SobreNosotros.vue'
+import ExploraAfrica from './components/ExploraAfrica.vue'
+import ContactoForm from './components/ContactoForm.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/explora', name: 'Explora', component: ExploraAfrica },
-  { path: '/reservar', name: 'Reservar', component: ContactoForm },
   { path: '/safari-playa', name: 'SafariPlaya', component: SafariPlaya },
+  { path: '/reservar', name: 'Reservar', component: ContactoForm },
   { path: '/sobre-nosotros', name: 'SobreNosotros', component: SobreNosotros }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  // AQUÍ ESTÁ LA SOLUCIÓN MÁGICA:
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() { return { top: 0 } }
 })
